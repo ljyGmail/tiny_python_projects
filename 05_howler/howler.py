48a1c2ddc22978b25050e535c20e0341e91eb87b
liangjinyong <liangjy881@gmail.com>
2023-12-27 19:10:06 +0900

    ch05 finished

---
+++ 05_howler/howler.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Author : macbookpro16 <macbookpro16@localhost>
+Date   : 2023-12-27
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import os
+import sys
+
+# --------------------------------------------------
+
+
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Howler (upper-cases input)',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text',
+                        metavar='text',
+                        help='Input string or file')
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='Output filename',
+                        metavar='str',
+                        type=str,
+                        default='')
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read().rstrip()
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    out_fh = open(args.outfile, 'wt') if args.outfile else sys.stdout
+    out_fh.write(args.text.upper() + '\n')
+    out_fh.close()
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
